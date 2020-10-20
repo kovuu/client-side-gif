dd77f5a131dbadbebd454e56dc8d8fa9c358961b
neatomaru <neatomaru@gmail.com>
2020-10-20 23:51:04 +0300

    Update connect.ts

--- src/consts/connect.ts
+++ src/consts/connect.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://polar-crag-51734.herokuapp.com';
-export const SERVER_NAME = 'polar-crag-51734';
+export const BASE_URL = 'https://blooming-atoll-32809.herokuapp.com';
+export const SERVER_NAME = 'blooming-atoll-32809';
